5a735d6b1ff997828cc6e791c16a6e45d6cc037f
Kuragna <abourrich@proton.me>
2023-03-03 16:29:03 +0100

    Add files via upload

--- get_next_line.c
+++ get_next_line.c
@@ -25,24 +25,21 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	return (str);
 }
 
-char	*get_line_buff(char *str)
+char	*get_line(char **ptr)
 {
 	char	*line;
-	int i;
-	int j;
+	int		i;
+	int		j;
 
-	j = 0;
 	i = 0;
-	while (str[i] != EOL && str[i] != '\0')
+	j = -1;
+	while ((*ptr)[i] != '\n' && (*ptr)[i] != '\0')
 		i++;
 	line = (char*)malloc(sizeof(char) * (i + 2));
 	if (!line)
 		return (NULL);
-	while (j < (i + 1) && str[j] != '\0')
-	{
-		line[j] = str[j];
-		j++;
-	}
+	while (++j < (i + 1))
+		line[j] = *(*ptr)++;
 	line[j] = '\0';
 	return (line);
 }
@@ -50,6 +47,9 @@ char	*get_line_buff(char *str)
 char	*ft_strchr(const char *s, int c)
 {
 	c = (unsigned char)c;
+
+	if (!s)
+		return (NULL);
 	while ((*s != '\0') && (*s != c))
 		s++;
 	if (*s == c)
@@ -57,21 +57,21 @@ char	*ft_strchr(const char *s, int c)
 	return (NULL);
 }
 
+
 char	*get_next_line(int fd)
 {
 	static char	*buffer = NULL;
 	char buff_read[BUFFER_SIZE + 1];
 	char *line;
 	int nbyte;
+	int nline;
 
-	if (fd < 0 || BUFFER_SIZE <= 0)
+	if (fd < 0 || fd >= 1000 || BUFFER_SIZE <= 0)
 		return (NULL);
 	nbyte = 1;
 	while (nbyte > 0)
 	{
 		nbyte = read(fd, buff_read, BUFFER_SIZE);
-		/*if (nbyte == -1)
-			return (NULL);*/
 		if (nbyte != 0)	
 		{
 			buff_read[nbyte] = '\0';
@@ -79,25 +79,29 @@ char	*get_next_line(int fd)
 		}
 	}
 	line = get_line_buff(buffer);
-	buffer = ft_strchr(buffer, '\n') + 1;
 	return (line);
 }
-
-/*int main()
+#if 0
+int main()
 {
 	atexit(find_leaks);
 	int fd = open("input.txt", O_RDONLY);
 	if (fd < 0)
 		printf("ERROR: could not open the file.");
 	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
 	close(fd);
 	return 0;
-}*/
-
-/*int main2()
+}
+#endif
+#if 0
+int main()
 {
 	atexit(find_leaks);
 	char	*buff = "abc\n123\n321\ncba";
-	get_new_buff(buff);
+	char	*line;
+	line = get_line_buff(buff);
+	printf("{line = %s}\n", line);
 	return 0;
-}*/
+}
+#endif
