f195ea3caea6971040e46bc822c6963ff6e02aa1
Kuragna <abourrich@proton.me>
2023-03-04 14:25:41 +0100

    pass "norminette" and check if file empty.

--- get_line.c
+++ get_line.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_line.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aabourri <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/04 14:21:37 by aabourri          #+#    #+#             */
+/*   Updated: 2023/03/04 14:22:48 by aabourri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdlib.h>
 
 char	*get_line(char **ptr)
@@ -8,7 +20,7 @@ char	*get_line(char **ptr)
 
 	i = 0;
 	j = -1;
-	if (*(*ptr) == '\0')
+	if ((*ptr) == NULL || *(*ptr) == '\0')
 		return (NULL);
 	while ((*ptr)[i] != '\n' && (*ptr)[i] != '\0')
 		i++;
@@ -16,7 +28,7 @@ char	*get_line(char **ptr)
 		i++;
 	else
 		i += 2;
-	line = (char*)malloc(sizeof(char) * i);
+	line = (char *)malloc(sizeof(char) * i);
 	if (!line)
 		return (NULL);
 	while (++j < (i - 1))
