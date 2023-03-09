a86c274d5e6d8078cede2568e9aeda9a4078b496
Kuragna <abourrich@proton.me>
2023-03-09 12:59:12 +0100

    almost pass all tester

--- get_next_line.c
+++ get_next_line.c
@@ -1,104 +1,115 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aabourri <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/08 15:01:41 by aabourri          #+#    #+#             */
+/*   Updated: 2023/03/08 18:29:03 by aabourri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "get_next_line.h"
-#include <string.h>
+#include <stdio.h>
 
-void	*ft_realloc(void *ptr, size_t new_size);
-
-void	find_leaks()
+void	find_leaks(void)
 {
 	system("leaks -q main");
 }
 
+char	*get_line_(char **buffer)
+{
+	char	*line;
+	char	*tmp;
+	size_t	i;
 
+	if (!*buffer || !**buffer)
+		return (free(*buffer), NULL);
+	i = 0;
+	while ((*buffer)[i] != '\n' && (*buffer)[i] != '\0')
+		i++;
+	if ((*buffer)[i] != '\0')
+		i++;
+	tmp = *buffer;
+	line = ft_substr(*buffer, 0, i);
+	*buffer = ft_substr(*buffer, i, ft_strlen(*buffer + i));
+	free(tmp);
+	tmp = NULL;
+	return (line);
+}
 
-char	*ft_strjoin(char *s1, char const *s2, char	**tmp)
+char	*get_join(char *s1, char const *s2)
 {
 	char	*str;
 	size_t	len_s1;
 	size_t	len_s2;
 
 	if (!s1)
+	{
+		s1 = malloc(sizeof(char));
+		if (s1 == NULL)
+			return (NULL);
 		len_s1 = 0;
+	}
 	else
-		len_s1 = strlen(s1);
-	len_s2 = strlen(s2);
-	str = (char*)malloc(sizeof(char) * len_s1 + len_s2 + 1);
+		len_s1 = ft_strlen(s1);
+	len_s2 = ft_strlen(s2);
+	str = malloc(sizeof *str * (len_s1 + len_s2 + 1));
 	if (!str)
 		return (NULL);
-	memmove(str, s1, len_s1);
-	memmove(str + len_s1, s2, len_s2);
-	str[len_s1 + len_s2] = '\0';
-	*tmp = str;
+	ft_memcpy(str, s1, len_s1);
+	ft_memcpy(str + len_s1, s2, len_s2);
+	str[len_s2 + len_s1] = '\0';
+	free(s1);
+	s1 = NULL;
 	return (str);
 }
 
-char	*get_line_buff(char **buffer)
-{
-	char	*line;
-	int		i;
-	int		j;
-
-	i = 0;
-	j = -1;
-	if (!*buffer || !**buffer)
-		return (NULL);
-	while ((*buffer)[i] != EOL && (*buffer)[i])
-		i++;
-	if (!(*buffer)[i])
-		i++;
-	else
-		i += 2;
-	line = (char*)malloc(sizeof(char) * i);
-	if (!line)
-		return (NULL);
-	while (++j < (i - 1))
-		line[j] = (*(*buffer)++);
-	line[j] = '\0';
-	return (line);
-}
-
-void	get_freed(char *ptr)
+char	*get_freed(char *ptr)
 {
 	if (ptr != NULL)
 	{
 		free(ptr);
 		ptr = NULL;
 	}
+	return (NULL);
 }
 
 char	*get_next_line(int fd)
 {
 	static char	*buffer;
-	char buff_read[BUFFER_SIZE + 1];
-	int nbyte;
-	char	*tmp;
-	char	*line;
+	char		buff_read[BUFFER_SIZE + 1];
+	int			nbyte;
 
-	if (fd < 0 || BUFFER_SIZE <= 0)
+	if (fd < 0  || BUFFER_SIZE <= 0)
 		return (NULL);
 	nbyte = 1;
 	while (nbyte > 0)
 	{
 		nbyte = read(fd, buff_read, BUFFER_SIZE);
-		if (nbyte > 0)	
+		if (nbyte == -1)
+			return (free(buffer), NULL);
+		if (nbyte > 0)
 		{
 			buff_read[nbyte] = '\0';
-			buffer = ft_strjoin(buffer, buff_read, &tmp);
-			get_freed(tmp);
-			if (strchr(buffer, EOL))
-                break ;
-        }
+			buffer = get_join(buffer, buff_read);
+			if (ft_strchr(buffer, '\n'))
+				break ;
+		}
 	}
-	line = get_line_buff(&buffer);
-	return (line);
+
+	return (get_line_(&buffer));
 }
-#if 0
+#if 0 
 int main()
 {
 	atexit(find_leaks);
 	int fd = open("input.txt", O_RDONLY);
 	if (fd < 0)
 		printf("ERROR: could not open the file.");
-	
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
 	printf("%s", get_next_line(fd));
 	printf("%s", get_next_line(fd));
 	printf("%s", get_next_line(fd));
@@ -107,29 +118,58 @@ int main()
 	return 0;
 }
 #endif
-#if 0
-int main()
+
+size_t	ft_strlen(const char *s)
 {
-	atexit(find_leaks);
-	char	*buff = "abc\n123\n321\ncba";
-	char	*line;
-	line = get_line_buff(buff);
-	printf("{line = %s}\n", line);
-	return 0;
+	size_t	len;
+
+	len = 0;
+	while (s[len] != '\0')
+		len++;
+	return (len);
 }
-#endif
 
-void	*ft_realloc(void *ptr, size_t new_size)
+char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	void	*new_ptr;
+	char	*str;
+	size_t	len_s;
 
-	if (!new_size || !ptr)
+	if (!s)
 		return (NULL);
-	new_ptr = malloc(new_size);
-	if (!new_ptr)
+	len_s = ft_strlen(s);
+	if (start > len_s)
+		len = 0;
+	if (len_s < len)
+		len = len_s - start;
+	str = (char *)malloc((len + 1) * sizeof(char));
+	if (!str)
 		return (NULL);
-	memmove(new_ptr, ptr, new_size);
-	free(ptr);
-	ptr = NULL;
-	return (new_ptr);
+	ft_memcpy(str, s + start, len);
+	str[len] = '\0';
+	return (str);
+}
+
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	size_t	i;
+
+	if (!dst && !src)
+		return (NULL);
+	i = 0;
+	while (i < n)
+	{
+		*((char *)dst + i) = *((char *)src + i);
+		i++;
+	}
+	return (dst);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	c = (unsigned char)c;
+	while ((*s != '\0') && (*s != c))
+		s++;
+	if (*s == c)
+		return ((char *)s);
+	return (NULL);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,26 +6,28 @@
 /*   By: aabourri <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/24 11:50:48 by aabourri          #+#    #+#             */
-/*   Updated: 2023/02/28 14:35:35 by aabourri         ###   ########.fr       */
+/*   Updated: 2023/03/08 18:18:09 by aabourri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NETX_LINE_H
-# define GET_NETX_LINE_H
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H 
 
-# include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
 # include <fcntl.h>
 
-#ifndef EOL
-#define EOL '\n'
-#endif
+# ifndef BUFFER_SIZE
+# define BUFFER_SIZE 10
+# endif
 
-#ifndef BUFFER_SIZE
-#define BUFFER_SIZE 1
-#endif
-
+char    *get_freed(char *ptr);
+char	*get_join(char *s1, char const *s2);
+char	*get_line_(char **buffer);
 char	*get_next_line(int fd);
-char	*get_line_len(int fd);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strchr(const char *s, int c);
+void	*ft_memcpy(void *dst, const void *src, size_t len);
+size_t	ft_strlen(const char *s);
+
 #endif
